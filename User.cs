cc70f277ac23eff3d55db0f4af5ce64662e0ec96
timbilding <144000698+timbilding@users.noreply.github.com>
2025-04-30 08:40:54 +0700

    Добавьте файлы проекта.

---
+++ User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceLibrary
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string FIO { get; set; }
+        public string TelegramNumber { get; set; }
+        public string TLUserName { get; set; }
+        public long TLChatId { get; set; }
+    }
+}
